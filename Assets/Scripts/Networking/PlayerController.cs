b95e5d4bab7455937c35d2b0ddf4b40bd47e3d07
val-web <sup1qu@gmail.com>
2025-05-16 20:47:58 +0200

    networking

--- Assets/Scripts/Networking/PlayerController.cs
+++ Assets/Scripts/Networking/PlayerController.cs
@@ -1,5 +1,6 @@
 using Unity.Netcode;
 using UnityEngine;
+using System.Collections;
 
 public class PlayerController : NetworkBehaviour
 {
@@ -11,6 +12,11 @@ public class PlayerController : NetworkBehaviour
 
     public GameObject boyModel;
     public GameObject girlModel;
+    public Animator animator;
+    public RuntimeAnimatorController boyController;
+    public RuntimeAnimatorController girlController;
+
+    private bool positionSet = false;
 
     public override void OnNetworkSpawn()
     {
@@ -27,6 +33,41 @@ public class PlayerController : NetworkBehaviour
         ApplyRole(role.Value);
     }
 
+    private void Start()
+    {
+        StartCoroutine(WaitAndSetSpawnPosition());
+    }
+
+    private IEnumerator WaitAndSetSpawnPosition()
+    {
+        while (!IsOwner || role.Value == CharacterType.Unassigned)
+        {
+            yield return null;
+        }
+
+        if (!positionSet)
+        {
+            SetSpawnPosition();
+            positionSet = true;
+        }
+    }
+
+    private void SetSpawnPosition()
+    {
+        string spawnName = role.Value == CharacterType.Boy ? "SpawnPoint_Boy" : "SpawnPoint_Girl";
+        GameObject spawnPoint = GameObject.Find(spawnName);
+
+        if (spawnPoint != null)
+        {
+            transform.position = spawnPoint.transform.position;
+            Debug.Log($"[CLIENT {OwnerClientId}] Spawned at {spawnName}");
+        }
+        else
+        {
+            Debug.LogWarning($"[CLIENT {OwnerClientId}] Could not find {spawnName}");
+        }
+    }
+
     private void ApplyRole(CharacterType r)
     {
         if (boyModel == null || girlModel == null)
@@ -37,6 +78,12 @@ public class PlayerController : NetworkBehaviour
 
         boyModel.SetActive(r == CharacterType.Boy);
         girlModel.SetActive(r == CharacterType.Girl);
-        Debug.Log($"[CLIENT {OwnerClientId}] I am {r}");
+
+        if (animator != null)
+        {
+            animator.runtimeAnimatorController = r == CharacterType.Boy ? boyController : girlController;
+        }
+
+        Debug.Log($"[CLIENT {OwnerClientId}] Role applied: {r}");
     }
 }
